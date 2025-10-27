605a163f6e7b02735cddac1f7588b16976c9a3fb
hanaaden <hanaadamabdi@gmail.com>
2025-10-27 21:22:19 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [name , setName] = useState("")
+  const [image , setImage] = useState("")
+  
+
+  const HandleFetch = async(i:number)=>{
+       const URL = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+       const res = await URL.json()
+       setName(res.name )
+       setImage(res.sprites.front_default)
+  }
+ useEffect(() => {
+  let i = 1; 
+  const interval = setInterval(() => {
+    if (i <= 100) { 
+      HandleFetch(i);
+      i++;
+    } else {
+      clearInterval(interval); 
+    }
+  }, 2000); 
+
+}, []);
+
+
+  return (
+    <>
+      <h1>Pokemon API</h1>
+       <div className='bg-gray-600 text-white rounded-2xl p-12 mt-12 git init text-center'>
+        <p className='text-6xl'>{name}</p>
+       <img className='w-48 md:w-60 h-48 md:h-60 mx-auto' src={image} alt={name}/>
+       </div>
+      
+    </>
+  )
+}
+
+export default App
