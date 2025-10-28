7826d4ce99b40d0b2bbfe3fde1e97cc4d2c797a7
hanaaden <hanaadamabdi@gmail.com>
2025-10-28 18:32:33 +0300

    more add

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   const [display, setDisplay] = useState(false);
   const [height, setHeight] = useState();
   const [weight, setWeight] = useState();
-  const [type, setType] = useState();
+  const [type, setType] = useState("");
   const [ability, setAbility] = useState();
 
   const handleEvent = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -56,6 +56,32 @@ function App() {
     HandleFetch();
   }, []);
 
+const getTypeColor = (type: string): string => {
+  const types: Record<string, string> = {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD",
+  };
+
+  return types[type.toLowerCase()] || "add any default color";
+};
+
+
   return (
     <>
       <h1 className="text-6xl">Pokemon API</h1>
@@ -66,7 +92,13 @@ function App() {
           <img className="w-48 h-48 " src={image} alt={name} />
           <p className="p-2 font-bold">Height : {height} </p>
           <p className="p-2 font-bold">weight : {weight}</p>
-          <p className="p-2 font-bold">types : {type}</p>
+          <p
+  className="p-2 font-bold rounded-full px-6 mt-2"
+  style={{ backgroundColor: getTypeColor(type) }}>
+Type: {type}
+</p>
+
+          
           <p className="p-2 font-bold">Ability : {ability}</p>
         </div>
       )}
